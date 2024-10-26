b1f7cf17ef7835124d5825727076c546c4c06430
McLaren Labs <info@mclarenlabs.com>
2024-10-26 07:35:18 -0700

    MidiTalk essentials

---
+++ Applications/MidiTalk/MidiTalk_AppDelegate.h
@@ -0,0 +1,55 @@
+/**
+ * MidiTalk - StepTalk scriptable MIDI synth thing
+ *
+ */
+
+#import <AppKit/AppKit.h>
+
+#import "McLarenSynthKit/McLarenSynthKit.h"
+#import "ASKSeqDispatcher.h"
+
+#import "NSScrollSlider.h"
+#import "MLPianoController.h"
+#import "MLGaugesController.h"
+#import "MLButtonsController.h"
+#import "MLFilterController.h"
+#import "MLReverbController.h"
+#import "MLContextController.h"
+
+@interface AppDelegate : NSObject<NSApplicationDelegate>
+
+@property (nonatomic, retain, strong) NSMenu *mainMenu;
+@property (nonatomic, retain, strong) NSWindow *mainWindow;
+
+@property (nonatomic, retain, strong) NSButton *startButton;
+@property (nonatomic, retain, strong) NSButton *stopButton;
+@property (nonatomic, retain, strong) NSButton *continueButton;
+@property (nonatomic, retain, strong) NSScrollSlider *tempoSlider;
+@property (nonatomic) int tempo;
+@property (nonatomic, retain, strong) NSTextField *tempoText;
+
+
+@property (nonatomic, retain, strong) MLGaugesController *gaugesController;
+@property (nonatomic, retain, strong) MLButtonsController *buttonsController;
+@property (nonatomic, retain, strong) MLPianoController *pianoController;
+
+// Audio Elements
+@property (nonatomic, retain, strong) MSKContext *ctx;
+@property (nonatomic, retain, strong) MSKContext *rec;
+@property (nonatomic, retain, strong) ASKSeq *seq;
+@property (nonatomic, retain, strong) ASKSeqDispatcher *disp;
+
+@property (nonatomic, retain, strong) MSKMetronome *metro;
+@property (nonatomic, retain, strong) MSKScheduler *sched;
+
+@property MLContextController *outputContextController;
+@property MLContextController *inputContextController;
+@property MLFilterController *filterController;
+@property MLReverbController *reverbController;
+
+@property (nonatomic, retain, strong) MSKFilterModel *filterModel;
+@property (nonatomic, retain, strong) MSKReverbModel *reverbModel;
+
+
+
+@end
